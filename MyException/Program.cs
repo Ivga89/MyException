f58c49a8ea42a63b6456404a2981ad3be5686a20
JhonnyGames <borozdinaes89@gmail.com>
2024-11-18 01:23:13 +0500

    MyException DZ+

--- MyException/Program.cs
+++ MyException/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Exception[] exceptions = new Exception[]
+        {
+            new MyException(), 
+            new ArgumentException(),
+            new DivideByZeroException(), 
+            new IndexOutOfRangeException(),
+            new InvalidOperationException()
+        };
+
+        foreach (Exception ex in exceptions)
+        {
+            try
+            {
+                throw ex;
+            }
+            catch (MyException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed.");
+            }
+        }
+    }
+}
+
+public class MyException : Exception
+{
+    public MyException() : base("My Exception") { }
+}
