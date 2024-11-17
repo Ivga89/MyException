72c879e148830962af19bba3433db419a4bdbc46
JhonnyGames <borozdinaes89@gmail.com>
2024-11-18 02:21:18 +0500

    like works

--- MyEvent/Program.cs
+++ MyEvent/Program.cs
@@ -1,10 +1,49 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         List<string> names = new List<string> { "Иванов", "Петров", "Сидоров", "Алексеев", "Борисов" };
 
-        
+        SortingEvents sortingEvents = new SortingEvents();
+
+        while (true)
+        {
+            try
+            {
+                int input = sortingEvents.Read();
+
+                switch (input)
+                {
+                    case 1:
+                        sortingEvents.OnSortEvent(names, SortOrder.Ascending);
+                        break;
+                    case 2:
+                        sortingEvents.OnSortEvent(names, SortOrder.Descending);
+                        break;
+                }
+
+                Console.WriteLine("Отсортированный список фамилий:");
+                foreach (var name in names)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+            catch (InvalidInputException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed.");
+            }
+        }
     }
 
     static void SortAscending(List<string> names)
@@ -17,11 +56,6 @@
         names.Sort();
         names.Reverse();
     }
-
-}
-
-    
-
 }
 
 public class InvalidInputException : Exception
@@ -29,14 +63,27 @@ public class InvalidInputException : Exception
     public InvalidInputException() : base("Invalid Input Exception") { }
 }
 
+public enum SortOrder
+{
+    Ascending,
+    Descending
+}
+
 public class SortingEvents
 {
     public delegate void SortEventHandler(List<string> names);
-    public event SortEventHandler SortEvent;
 
-    public void OnSortEvent(List<string> names)
+    public void OnSortEvent(List<string> names, SortOrder order)
     {
-        SortEvent?.Invoke(names);
+        switch (order)
+        {
+            case SortOrder.Ascending:
+                SortAscending(names);
+                break;
+            case SortOrder.Descending:
+                SortDescending(names);
+                break;
+        }
     }
 
     public int Read()
@@ -46,5 +93,15 @@ public class SortingEvents
         if (input != 1 && input != 2) throw new InvalidInputException();
         return input;
     }
-}
 
+    private void SortAscending(List<string> names)
+    {
+        names.Sort();
+    }
+
+    private void SortDescending(List<string> names)
+    {
+        names.Sort();
+        names.Reverse();
+    }
+}
